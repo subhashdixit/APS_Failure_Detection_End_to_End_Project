b28690119a4072585601919f0a13da39edfb2a16
Subhash Dixit <subhashdixit17@gmail.com>
2022-12-02 23:42:12 +0530

    updated logger and exception file

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-import pymongo
+# import pymongo
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+# # Provide the mongodb localhost url to connect python to mongodb.
+# client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-# Database Name
-dataBase = client["neurolabDB"]
+# # Database Name
+# dataBase = client["neurolabDB"]
 
-# Collection  Name
-collection = dataBase['Products']
+# # Collection  Name
+# collection = dataBase['Products']
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
+# # Sample data
+# d = {'companyName': 'iNeuron',
+#      'product': 'Affordable AI',
+#      'courseOffered': 'Machine Learning with Deployment'}
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
+# # Insert above records in the collection
+# rec = collection.insert_one(d)
 
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
+# # Lets Verify all the record at once present in the record with all the fields
+# all_record = collection.find()
 
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+# # Printing all records present in the collection
+# for idx, record in enumerate(all_record):
+#      print(f"{idx}: {record}")
+
+"""Just For TesTing Purpose oof logger and exception"""
+# from sensor.logger import logging
+# from sensor.exception import SensorException
+# import sys, os
+
+# def test_logger_and_exception():
+#      try : 
+#           logging.info("Staring the test_logger_and_exception")
+#           result = 3/0
+#           print(result)
+#           logging.info("Stopping the test_logger_and_exception")
+#      except Exception as e:
+#           logging.debug(str(e))
+#           raise SensorException(e, sys)
+
+# if __name__ == "__main__" :
+#      try:
+#           test_logger_and_exception()
+#      except Exception as e:
+#           print(e)
--- sensor/logger.py
+++ sensor/logger.py
@@ -1,23 +1,25 @@
 #Execution details will be present
+# Order of execution CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET
 import logging
 import os
 from datetime import datetime
+import os
 
-#Name of the file
-LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H_%M_%S')}.log"
+#log file name
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
-# Directory Name
-LOG_FILE_DIR = os.path.join(os.getcwd(), "logs")
+#log directory
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
-#Create folder if not available
-os.makedirs(LOG_FILE_DIR, exist_ok=True)
+#create folder if not available
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
-#Log file path
-LOG_FILE_PATH = os.path.join(LOG_FILE_DIR, LOG_FILE_NAME)
+#log file path
 
-#Configure logging
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
